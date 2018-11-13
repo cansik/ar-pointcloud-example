f511ecac50862f4d486e3cbbef391a74439778b5
Florian Bruggisser <florian@bildspur.ch>
2018-11-13 12:37:06 +0100

    added example ar project based on metal

---
+++ ARPointCloudRenderer/ARPointCloudRenderer/ViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ViewController.swift
+//  ARPointCloudRenderer
+//
+//  Created by Florian Bruggisser on 13.11.18.
+//  Copyright © 2018 Florian Bruggisser. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = createScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // allows the user to manipulate the camera
+        sceneView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // configure the view
+        sceneView.backgroundColor = UIColor.black
+    }
+    
+    func createScene() -> SCNScene
+    {
+        // create a new scene
+        let scene = SCNScene()
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.camera?.zNear = 0.0
+        cameraNode.camera?.zFar = 10.0
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 0.3)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 3, z: 3)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.white
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        // retrieve the node
+        let pc = PointCloud()
+        let pcNode = pc.getNode()
+        pcNode.position = SCNVector3(x: 0, y: 0, z: 0)
+        
+        scene.rootNode.addChildNode(pcNode)
+        
+        // animate the 3d object
+        pcNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.1, z: 0, duration: 1)))
+        return scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
